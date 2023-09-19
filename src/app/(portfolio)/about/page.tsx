10a0606353163c2bfee38b92b7d8218dc3e706e2
= <mae.vdb@outlook.com>
2023-09-19 19:02:51 +0200

    fix colors and content for the portfolio about page

--- src/app/(portfolio)/about/page.tsx
+++ src/app/(portfolio)/about/page.tsx
@@ -10,7 +10,7 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons';
-import portraitImage from '@/images/portrait.jpg';
+import portraitImage from '@/images/portrait.png';
 
 function SocialLink({
   className,
@@ -27,9 +27,9 @@ function SocialLink({
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-main-color dark:text-zinc-200 dark:hover:text-secondary-color"
       >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-main-color group-hover:dark:fill-secondary-color" />
         <span className="ml-4">{children}</span>
       </Link>
     </li>
@@ -50,7 +50,7 @@ function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 export const metadata: Metadata = {
   title: 'About',
   description:
-    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+    'I’m Mae van den Bergh. I live in New York City, where I design the future.',
 };
 
 export default function About() {
@@ -69,7 +69,7 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Spencer Sharp. I live in New York City, where I design the
+            I’m Mae van den Bergh. I live in New York City, where I design the
             future.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
@@ -116,11 +116,11 @@ export default function About() {
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:spencer@planetaria.tech"
+              href="mailto:mae.vdb@outlook.com"
               icon={MailIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
-              spencer@planetaria.tech
+              mae.vdb@outlook.com
             </SocialLink>
           </ul>
         </div>
