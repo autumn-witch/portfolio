7b6e857318725c471b10b6ab1f2e7cecd959c74a
= <mae.vdb@outlook.com>
2023-09-19 19:01:57 +0200

    replace spencer with mae in the portfolio layout

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,18 +1,18 @@
 import { type Metadata } from 'next';
 
-import { Providers } from '@/app/(portfolio)/providers';
+import { Providers } from '@/app/providers';
 import { Layout } from '@/modules/Portfolio/Layout';
 
 import '@/styles/tailwind.css';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Mae van den Bergh',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Mae van den Bergh - Software designer, founder, and amateur astronaut',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Mae, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
