76b4e71ae343cc662e96c984168f839afa3f161b
= <mae.vdb@outlook.com>
2023-09-20 11:51:07 +0200

    fix remove unused link in the footer

--- src/modules/Portfolio/misc.ts
+++ src/modules/Portfolio/misc.ts
@@ -22,6 +22,3 @@ export const links: Array<LinkObject> = [
     name: 'Uses',
   },
 ];
-
-export const catchPhrase = `Hello! I'm Mae, a self-taught web developer who loves JavaScript. I strongly believe in a human and accessible web. \n
-On my free time, aside from writing web applications, I love to read, I'm a LGBTQIA+ activist, and lately I've been trying to learn how to cook as well as play piano.`;
