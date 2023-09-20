bcaf8fb1f206ca849de2b36a14665fd819df91ee
= <mae.vdb@outlook.com>
2023-09-20 11:48:11 +0200

    refactor the about page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -78,30 +78,33 @@ export default function About() {
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              From the youngest age, I've been a very curious and persevering
+              individual. Once something caught my interest, I'd see it as a
+              puzzle and I would go to great lengths to solve it.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              At the age of 14, I injured myself pretty badly. That led me to
+              spend a lot of time behind our family computer, discovering the
+              web with an insatiable curiosity.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              I spent the next two years becoming versed in the amazing world of
+              computers, and ended up discovering programming. This was such an
+              eye-opening experience that I enrolled to a computer high school
+              course.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              I spent the following years figuring out one of life's hardest
+              problems: understanding myself. At the age of 25, I finally found
+              the answer: I am a non-binary folk who simply <em>loves</em>{' '}
+              solving puzzles, no matter their shape. I also take
+            </p>
+            <p>
+              This led to finding an internship at{' '}
+              <ExternalLink href="https://en.fabriq.tech/" content="Fabriq" />,
+              during which I learned a lot of how it all works. I currently am
+              preparing some amazing stuff (such as <em>Notes en Bulle</em>), so
+              stay tuned!
             </p>
           </div>
         </div>
