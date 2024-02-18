95ba5823708f2d61a54e46142abcef68cae9397e
= <mae.vdb@outlook.com>
2024-02-18 13:27:07 +0100

    remove the /uses page

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -17,10 +17,6 @@ export const links: Array<LinkObject> = [
     path: '/projects',
     name: 'Projects',
   },
-  {
-    path: '/uses',
-    name: 'Uses',
-  },
 ];
 
 export function clamp(number: number, a: number, b: number) {
@@ -29,21 +25,24 @@ export function clamp(number: number, a: number, b: number) {
   return Math.min(Math.max(number, min), max);
 }
 
-export const catchPhraseString = `Hello! I'm Mae, a self-taught web developer who loves JavaScript.
+export const catchPhraseString = `Hello! I'm Mae, a non-binary self-taught web developer who loves JavaScript.
 	I strongly believe in a human and accessible web.
 	On my free time, aside from writing web applications, I love to read,
 	I'm a LGBTQIA+ activist, and lately I've been trying to learn how to
 	cook as well as play piano.`;
 
 /**
- * ? NEXT STEPS
+ * ! NEXT STEPS
  * TODO REWRITE THE ABOUT SECTION
  * TODO CHECK THE USES SECTION
- * TODO ADD A11Y
- * TODO ADD I18N
+ * TODO FIX A11Y CURRENT PROBLEMS
+ * TODO FIX DARK THEME AUTO-SET
+ * TODO CHECK IF THERE ARE OTHERS A11Y ISSUES; IF SO, FIX THEM
+ * TODO ADD I18N (content + handling routes and change locale)
  * TODO WRITE ONE OR TWO ARTICLES AND USE THEM
  * TODO FOLLOW SEO COURSES AND USE THE KNOWLEDGE IN THE APP
  * TODO UPDATE THE README FILE
  * TODO DO ONE LAST CHECK
  * TODO DEPLOY
+ * ? SHARE THE URL?
  */
