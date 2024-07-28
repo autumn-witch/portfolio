09d6d96c0b70a65dcb06f1281d4d690b47a0163e
= <mae.vdb@outlook.com>
2024-07-28 19:50:43 +0200

    cleanup footer base component

--- src/BaseComponents/Footer.tsx
+++ src/BaseComponents/Footer.tsx
@@ -34,7 +34,7 @@ export function Footer() {
                 ))}
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Autumn Witch. ABRACACABRA
+                &copy; {new Date().getFullYear()} Autumn Witch
               </p>
             </div>
           </ContainerInner>
