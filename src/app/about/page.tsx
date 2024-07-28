bc271ca96797197d26efd84a66b012acf60e1069
= <mae.vdb@outlook.com>
2024-07-28 19:49:34 +0200

    cleanup about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,12 @@
 import { type Metadata } from 'next';
 import Image from 'next/image';
-import Link from 'next/link';
-import clsx from 'clsx';
 
 import { Container } from '@/BaseComponents/Container';
 
-import { SocialLink } from '@/BaseComponents/SocialLink';
-import { SimpleMailIcon as MailIcon } from '@/BaseComponents/Icons/MailIcon';
-import { GitHubIcon } from '@/BaseComponents/Icons/GitHubIcon';
-import { ExternalLink } from '@/BaseComponents/ExternalLink';
 import { Catchphrase } from '@/BaseComponents/Catchphrase';
+import portraitImage from '@/images/portrait.png';
 
 import { catchPhraseString } from '@/lib/misc';
-
-import portraitImage from '@/images/portrait.png';
 
 export const metadata: Metadata = {
   title: 'À propos',
@@ -27,6 +20,7 @@ export default function About() {
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:max-w-none">
+            {/* Add an alt text below */}
             <Image
               src={portraitImage}
               alt=""
@@ -40,19 +34,8 @@ export default function About() {
             <Catchphrase hasHeader />
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-            <p>Action directe.</p>
+            <Catchphrase hasInfos />
           </div>
-        </div>
-        <div className="lg:pl-20">
-          {/* <ul role="list">
-            <SocialLink
-              href="mailto:contact@autumnwit.ch"
-              icon={MailIcon}
-              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-            >
-              contact@autumnwit.ch
-            </SocialLink>
-          </ul> */}
         </div>
       </div>
     </Container>
