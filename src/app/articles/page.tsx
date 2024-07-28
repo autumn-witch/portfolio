948c982e98614e5ba3288e77ae7729d69d116c6d
= <mae.vdb@outlook.com>
2024-07-28 19:49:47 +0200

    cleanup articles page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -3,21 +3,21 @@ import { type Metadata } from 'next';
 import { SimpleLayout } from '@/BaseComponents/SimpleLayout';
 
 import { getAllArticles } from '@/modules/articles/articles';
-import { ArticleHorizontalPreview as ArticlePreview } from '@/modules/articles/ArticlePreview';
+import { ArticleHorizontalPreview } from '@/modules/articles/ArticlePreview';
 
 export const metadata: Metadata = {
-  title: 'ACAB',
-  description: 'Même ton père.',
+  title: 'Articles',
+  description: 'Liste de tous mes articles.',
 };
 
 export default async function ArticlesIndex() {
   const articles = await getAllArticles();
   return (
-    <SimpleLayout title="ACAB" intro="Même ton père.">
+    <SimpleLayout title="Articles" intro="Tous mes articles.">
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
           {articles.map((article) => (
-            <ArticlePreview key={article.slug} article={article} />
+            <ArticleHorizontalPreview key={article.slug} article={article} />
           ))}
         </div>
       </div>
