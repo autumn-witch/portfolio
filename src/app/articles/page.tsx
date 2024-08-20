07b317e71e270f02b160a65c291e3de7ed1d0457
= <mae.vdb@outlook.com>
2024-08-20 14:17:46 +0200

    pti refacto de l'intro dans /articles

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next';
 
 import { SimpleLayout } from '@/BaseComponents/SimpleLayout';
+import { WebLink } from '@/BaseComponents/WebLink';
 
 import { getAllArticles } from '@/modules/articles/articles';
 import { ArticleHorizontalPreview } from '@/modules/articles/ArticlePreview';
@@ -10,10 +11,24 @@ export const metadata: Metadata = {
   description: 'Liste de tous mes articles.',
 };
 
+function Intro() {
+  const baseUrl = process.env.PUBLIC_SITE_URL;
+  if (!baseUrl) {
+    throw Error('Missing PUBLIC_SITE_URL environment variable');
+  }
+  const url = `${baseUrl}/articles/feed.xml`;
+  return (
+    <>
+      <span>Tous mes articles. </span>
+      <WebLink href={url} content="Flux RSS" />
+    </>
+  );
+}
+
 export default async function ArticlesIndex() {
   const articles = await getAllArticles();
   return (
-    <SimpleLayout title="Articles" intro="Tous mes articles.">
+    <SimpleLayout title="Articles" intro={Intro()}>
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
           {articles.map((article) => (
