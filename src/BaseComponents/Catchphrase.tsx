75f234fea1375a3f2f5fdcd2038acc6f8dd17839
= <mae.vdb@outlook.com>
2024-08-31 14:30:44 +0200

    refactor du composant Catchphrase

--- src/BaseComponents/Catchphrase.tsx
+++ src/BaseComponents/Catchphrase.tsx
@@ -10,8 +10,16 @@ export function Catchphrase({
 }) {
   return (
     <section {...props}>
-      {hasHeader && <div>Sorcière d&apos;automne.</div>}
-      {hasInfos && <div>{catchPhraseString}</div>}
+      {hasHeader && (
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          Sorcière d&apos;automne.
+        </h1>
+      )}
+      {hasInfos && (
+        <p className="mt-6 whitespace-pre-line text-zinc-600 dark:text-zinc-200">
+          {catchPhraseString}
+        </p>
+      )}
     </section>
   );
 }
